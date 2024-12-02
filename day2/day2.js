81550602709d44c65a129d8ace3c0462adeb726b
kavyashivannagari <shivannagarikavya@gmail.com>
2024-12-02 17:18:16 +0530

    js task

---
+++ day2/day2.js
@@ -0,0 +1,53 @@
+// voting eligibility
+
+var age=prompt("enter your age");
+if(age>=18){
+    console.log("you are eligible to vote")
+}else{
+    console.log("you are not eligible to vote")
+}
+
+
+
+// marriage eligibility
+
+var male_age=prompt("enter male  age");
+var female_age=prompt("enter female age");
+if(male_age>=21 && female_age>=18){
+    console.log("you are eligible to marry")
+    }else{
+    console.log("you are not eligible marry")
+}
+
+
+
+// driving lisence
+
+var age=prompt("enter your age");
+if(age>=18){
+    console.log("you are eligible to apply driving lisence")
+}else{
+    console.log(" sorry!! you are not eligible to apply driving lisence ")
+}
+
+
+
+// attendence percentage
+
+var percentage=prompt("enter your percentage");
+if(percentage>=75){
+    console.log("you are eligible to write the exams")
+}
+else{
+    console.log("sorry!! you are not eligible to write the exams");
+}
+
+
+// senior citizen eligibility
+
+var age=prompt("enter your age");
+if(age>=60){
+    console.log("you are eligible for senior citizen discount")
+}else{
+    console.log(" sorry!! you are  not eligible for senior citizen discount ")
+}
