0a79620c2021c72fd269f1bc1935009f6f279509
kavyashivannagari <shivannagarikavya@gmail.com>
2024-12-13 14:34:29 +0530

    DOM Task

---
+++ DOM/dom.js
@@ -0,0 +1,31 @@
+ let h1tag=document.getElementById("h1")
+ 
+ function textChangeOnclick(){
+    h1tag.textContent="Unemployed individual who looking for a job";
+    h1tag.style.backgroundColor="lightcoral";
+    h1tag.style.cursor="pointer"
+     h1tag.style.borderRadius="50px"
+    h1tag.style.padding="60px"
+    h1tag.style.boxShadow="10px 10px 10px black"
+
+ }
+ let h2tag=document.getElementById("h2")
+ function textChangeDblclick(){
+    h2tag.textContent="True progress is slow but sure";
+    h2tag.style.backgroundColor="lightblue";
+    h2tag.style.cursor="pointer"
+    h2tag.style.borderRadius="50px"
+    h2tag.style.padding="60px"
+    h2tag.style.boxShadow="10px 10px 10px black"
+
+ }
+ let h3tag=document.getElementById("h3")
+ function textChangeMouseOver(){
+    h3tag.textContent="If you want to learn no one can stop you if you want to stop no one can help you";
+    h3tag.style.backgroundColor="lightgrey";
+    h3tag.style.cursor="pointer"
+    h3tag.style.borderRadius="50px"
+    h3tag.style.padding="60px"
+    h3tag.style.boxShadow="10px 10px 10px black"
+
+ }
